e8b61f6b5341a362e5f55c5a742037262b891a11
xiaguiwei <1185215113@qq.com>
2017-08-15 00:02:14 +0800

    app.js have some errors

--- app.js
+++ app.js
@@ -5,13 +5,57 @@ App({
     // var logs = wx.getStorageSync('logs') || []
     // logs.unshift(Date.now())
     // wx.setStorageSync('logs', logs)
+    var that=this
+    that.getUserInfo(function(userInfo){
+
+    })
     wx.checkSession({
-      success:function(res){
-        console.log(res)
+      success:function(){
+        console.log('ok')
+        //session未过期 
+        this.userLogin;
+      },
+      fail:function(){
+        console.log('fail')
+        // 登录态过期 重新请求
+        that.userLogin;
       }
     })
   },
-
+  //用户登录
+  userLogin:function(){
+    console.log('111')
+  	wx.login({
+        	success:function(res){
+        		if(res.code){
+        			//获取code 把code当成参数传到后台请求API
+        			wx.request({
+        				url:app.globalData.httpsurl+'public/index.php?s=privateinfo/wologin',
+        				data:{
+        					code:res.code,
+        				},
+        				success:function(res){
+        					//设置缓存session----reuserinfo
+        					try{
+        						wx.setStorageSync('reuserinfo',res.data)
+        					}catch(e){
+        						//
+        					}
+                },
+                fail:function(){
+                  wx.showModal({
+                    title:"提示",content:"用户不存在"
+                  })
+                }
+        			})
+        		}else{
+        			wx.showModal({
+                    title: "提示", content: "获取用户登录失败"+res.errMsg
+                  })
+        		}
+        	}
+        })
+  },
   getUserInfo: function(cb) {
     var that = this
     if (this.globalData.userInfo) {
@@ -23,12 +67,18 @@ App({
         success: function(res) {
           that.globalData.userInfo = res.userInfo
           typeof cb == "function" && cb(that.globalData.userInfo)
+        },
+        fail:function(res){
+        	var param={nickname:'未授权'}
+        	that.globalData.userInfo=param;
+        	typeof cb=="function" && cb(that.globalData.userInfo);
         }
       })
     }
   },
-
+  //定义全局变量
   globalData: {
-    userInfo: null
+    userInfo: null,
+    httpsurl:"http://xgwshenxcx.com/",
   }
 })
